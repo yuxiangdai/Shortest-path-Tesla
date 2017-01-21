c23033fcde06661876dcad88164080a126d3fb06
Daniel Guo <danielguo123@hotmail.com>
2017-01-21 15:12:46 -0500

    GOOD PART 2 - updated keys

--- get_info.py
+++ get_info.py
@@ -4,7 +4,7 @@ def get_info(lat1, lon1, lat2, lon2):
     import json
     from pprint import pprint
     
-    keys = ["AIzaSyB3yUcVCIRKw6as2WbrAReszeqTlELP1dA"]
+    keys = ["AIzaSyB3yUcVCIRKw6as2WbrAReszeqTlELP1dA","AIzaSyCgkQH8kVHzZ-nWyz9uazvwq9L5CY0qvL8","IzaSyDdRaFyIOzwPMwENHQVgrgr6DKzKYbnsNc","AIzaSyAAUAxUbaOFJ-pfx7epLMHfZ04wFrg-JMs","AIzaSyA9tVDmhB76ox5V5NuOxA2nXsix9T0vroQ","AIzaSyCT53zc5BiWIvLn5rkWJSz3F3JOU5paX1o","AIzaSyBxl-BjkVOFJ2SHy6YA0VrWgtuauMjE1dw","AIzaSyAyBVyYJtQO0v4jIuN2RR4Cj-99_rmZ6NQ","AIzaSyCwwiq6Z_gkFYPHZiwAvOq6S07DCPalB8I","AIzaSyAUuV53ZWF9l2JzangdtpQHtc_3WLGhhaY"]
     for key in keys:
         url_input = "origins="+str(lat1)+','+str(lon1) + "&destinations="+str(lat2)+','+str(lon2) + "&key="+key        
         url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=si&"+url_input
