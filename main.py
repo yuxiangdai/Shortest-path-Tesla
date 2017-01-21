b7b74c59bdc03b20607087f871bae4812bcf4fcf
ColinQiyangLi <colin.qiyang.li@gmail.com>
2017-01-21 18:42:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import json
-import path_finding
-import queue
 import caching
 
 with open('charging_stations.json') as data_file:    
@@ -26,27 +24,11 @@ def dist(lat1, lon1, lat2, lon2):
     d = R * c
     return d/1000.0
 
-
 def get_keys():
     keys = []
     for key in sta_info:
         keys.append(key)
-    return keys
-
-def get_keys_plus():
-    keys = []
-    for i in range(len(sts)):
-        keys.append(i)
-    return keys
-
-def filter_keys(lat, lon, keys):
-    global sts, sta_info
-    res_keys = []
-    for key in keys:
-        if dist(lat, lon, sta_info[sts[key]]['latitude'], sta_info[sts[key]]['longitude']) <= 480:
-            res_keys.append(key)
-    return res_keys
-                                                        
+    return keys                               
 
 def get_sort_keys_by_spherical_dist(lat, lon, keys):
     
@@ -58,27 +40,6 @@ def get_sort_keys_by_spherical_dist(lat, lon, keys):
             if t_dist_i > t_dist_j:
                 keys[i], keys[j] = keys[j], keys[i]
     return keys
-
-def get_sort_keys_by_spherical_dist_plus(lat, lon, keys):
-    
-    global sts, sta_info
-    for i in range(len(keys)):
-        for j in range(i+1, len(keys)):
-            t_dist_i = dist(sta_info[sts[keys[i]]]['latitude'], sta_info[sts[keys[i]]]['longitude'], lat, lon)
-            t_dist_j = dist(sta_info[sts[keys[j]]]['latitude'], sta_info[sts[keys[j]]]['longitude'], lat, lon)
-            if t_dist_i > t_dist_j:
-                keys[i], keys[j] = keys[j], keys[i]
-    return keys
-
-
-
-def get_top_k(lat, lon, lat_g, lon_g, k):
-    global sts, sta_info
-    keys = get_sort_keys_by_spherical_dist_plus(lat_g, lon_g, filter_keys(lat, lon, get_keys_plus()))
-    res = []
-    for i in range(len(keys)):
-        res.append(keys[i])
-    return res
 
 def print_top_3(lat, lon):
     global sts, sta_info
@@ -95,12 +56,6 @@ def get_dist(st1, st2):
     global sts, sta_info
     return dist(sta_info[st1]['latitude'], sta_info[st1]['longitude'], sta_info[st2]['latitude'], sta_info[st2]['longitude'])
 
-def get_closer(st1, st2, st3):
-    if get_dist(st1, st3) > get_dist(st2, st3):
-        return True
-    else:
-        return False
-
 def initialization():
     global sts, sta_info
     sts = []
@@ -109,70 +64,6 @@ def initialization():
     sts.append(0)
     sts.append(1)
 
-
-def SPFA():
-    global sts, sta_info
-    
-    end_pt = len(sts) - 1
-    start_pt = len(sts) - 2
-    node_num = len(sts)
-    MAX = 10000
-    total_dist = []
-    
-    graph = []
-    in_queue = []
-    shortest_time = []
-    total_dist = []
-    back_track = []
-    for i in range(node_num):
-        temp = []
-        shortest_time.append(MAX)
-        total_dist.append(0)
-        back_track.append(-1)
-        in_queue.append(False)
-        for j in range(node_num):
-            temp.append(0)
-        graph.append(temp)
-
-    q = queue.Queue()
-    q.put(start_pt)
-    print(start_pt)
-    print(node_num)
-    in_queue[start_pt] = True
-    shortest_time[start_pt] = 0
-    cnt = 3000
-    while q.empty() != True and cnt > 0:
-        cnt = cnt - 1
-        curr = q.get()
-        print("cnt:"+str(cnt))
-        print(shortest_time[curr])
-        print(total_dist[curr])
-        in_queue[curr] = False
-        keys = get_top_k(sta_info[sts[curr]]['latitude'], sta_info[sts[curr]]['longitude'], sta_info[sts[end_pt]]['latitude'], sta_info[sts[end_pt]]['longitude'], 5)
-        
-        print(keys)
-        for i in keys:
-            if i != curr and get_dist(sts[curr], sts[i]) <= 480 and get_closer(sts[curr], sts[i], sts[len(sts)-1]) == True:
-                dis, time = get_API_info(sts[curr], sts[i])
-                if dis <= 480 and (dis / 80.0 + dis / 480.0 / 3.0) + shortest_time[curr] < shortest_time[i]:
-                    shortest_time[i] = (dis / 80.0 + dis / 480.0 / 3.0) + shortest_time[curr]
-                    total_dist[i] = total_dist[curr] + dis
-                    back_track[i] = curr
-                    if in_queue[i] != True:
-                        in_queue[i] = True
-                        q.put(i)
-    temp_pt = end_pt
-    path = [sts[end_pt]]
-    while back_track[temp_pt]!=-1:
-        temp_pt = back_track[temp_pt]
-        t_id = sts[temp_pt]
-        path = path.append(t_id)
-    
-    for i in range(len(path)):
-        t_id = path[len(path)-i-1]
-        first_part = str(sta_info[t_id]['latitude']) + " " + str(sta_info[t_id]['longitude']) + " " + sta_info[t_id]['city']
-        print(first_part + " " + str(t_id))
-        
 def Dijkstra():
     global sts, sta_info
     
@@ -265,15 +156,17 @@ for i in range(number_of_results):
     sta_info[data['fuel_stations'][i]['id']] = sta_temp
 
 
+
 # Part 1
-'''
+print("Part 1:")
 coord_input = input('Enter your input:').split()
 coord_input = (float(coord_input[0]), float(coord_input[1]))
 print(coord_input)
 print_top_3(coord_input[0], coord_input[1])
-'''
+
 # Part 2
-'''
+
+print("Part 2:")
 start = input('Enter your input:').split()
 start = (float(start[0]), float(start[1]))
 
@@ -282,9 +175,10 @@ end = (float(end[0]), float(end[1]))
 
 dis, time = caching.request_google(start[0], start[1], end[0], end[1])
 print("%.3f" % dis, "%.2f" % time)
-'''
+
 # Part 4
 
+print("Part 4:")
 start = input('Enter your input:').split()
 start = (float(start[0]), float(start[1]))
 
