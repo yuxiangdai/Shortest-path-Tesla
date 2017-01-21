07e4e8559b6e023e2ef7e180ee48199695c44cab
ColinQiyangLi <colin.qiyang.li@gmail.com>
2017-01-21 15:31:03 -0500

    Finish part 1
    Problem: the distance is not exactly accurate

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ with open('charging_stations.json') as data_file:
     data = json.load(data_file)
 
 #When it asks for input, paste this: 38.8977 -77.0365
-coord_input = map(float,raw_input('Enter your input:').split())
-print coord_input
+coord_input = input('Enter your input:').split()
+coord_input = (float(coord_input[0]), float(coord_input[1]))
+print(coord_input)
 
 def dist(lat1, lon1, lat2, lon2):
     import math
-    R = 6371000.0
+    R = 6371000.0000
     lat1 = math.radians(lat1)
     lat2 = math.radians(lat2)
     lon1 = math.radians(lon1)
@@ -20,23 +21,39 @@ def dist(lat1, lon1, lat2, lon2):
     
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     d = R * c
-    return d
+    return d/1000.0
 
+def get_sort_keys_by_spherical_dist(lat, lon, sta_info):
+    keys = []
+    for key in sta_info:
+        keys.append(key)
+    for i in range(len(keys)):
+        for j in range(i+1, len(keys)):
+            t_dist_i = dist(sta_info[keys[i]]['latitude'], sta_info[keys[i]]['longitude'], lat, lon)
+            t_dist_j = dist(sta_info[keys[j]]['latitude'], sta_info[keys[j]]['longitude'], lat, lon)
+            if t_dist_i > t_dist_j:
+                keys[i], keys[j] = keys[j], keys[i]
+    return keys
+
+def print_top_3(lat, lon, sta_info):
+    keys = get_sort_keys_by_spherical_dist(lat, lon, sta_info)
+    #39.0250017 -77.148335 Westfield Montgomery Mall - Tesla 17.137
+    for i in range(3):
+        print(str(sta_info[keys[i]]['latitude']) + " " + str(sta_info[keys[i]]['longitude']) + " " + sta_info[keys[i]]['station_name'] + " " + "%.3f" % dist(sta_info[keys[i]]['latitude'], sta_info[keys[i]]['longitude'], lat, lon))
+    
 # get lat,lon,city, station name, id number_of_results
 
 number_of_results = data['total_results']
-print "Number of Results: " + str(number_of_results)
-num = 0
 
-print data['fuel_stations'][336]['latitude']
-print data['fuel_stations'][336]['longitude']
+sta_info = {}
 
-
-
-for i in range(number_of_results - 3, number_of_results - 1):
-    print dist(coord_input[0], coord_input[1], data['fuel_stations'][i]['latitude'],data['fuel_stations'][i]['longitude'])
-    
-    ##currently prints distance, make it make some fancy dictionary!!
+for i in range(number_of_results):
+    sta_temp = {}
+    sta_temp["latitude"] = data['fuel_stations'][i]['latitude']
+    sta_temp["longitude"] = data['fuel_stations'][i]['longitude']
+    sta_temp["station_name"] = data['fuel_stations'][i]['station_name']
+    sta_temp["city"] = data['fuel_stations'][i]['city']
+    sta_info[data['fuel_stations'][i]['id']] = sta_temp
     
 
-print num
+print_top_3(coord_input[0], coord_input[1], sta_info)
